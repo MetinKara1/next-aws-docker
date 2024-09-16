b210f244857a790787564d944fa0cb799526667e
MetinKara1 <n.metinkara@gmail.com>
2024-09-17 00:00:43 +0300

    chore: build fix

--- src/app/preload-resources.tsx
+++ src/app/preload-resources.tsx
@@ -3,6 +3,7 @@
 import ReactDOM from "react-dom";
 
 export function PreloadResources() {
+  // @ts-ignore
   const head = ReactDOM.createRoot(document.head);
 
   head.render(<link rel="preload" as="image" href="test-image.com" />);
