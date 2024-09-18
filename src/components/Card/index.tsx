e140ea5ef9da85de3259a38221d1394c3ac02a49
MetinKara1 <n.metinkara@gmail.com>
2024-09-18 22:59:26 +0300

    feat: style update

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -26,53 +26,57 @@ const Card = (props: { item: CardProps }) => {
   return (
     <div className="w-full h-full">
       <div className="flex flex-col justify-between border-0 rounded-xl bg-white mobile:p-4 tablet:p-6 w-full h-full">
-        <div className="flex justify-between">
-          <p className="font-bold text-lg">{item?.name}</p>
-          <div className="cursor-pointer">
-            <HeartIcon fill="#ED3F3F" />
+        <div>
+          <div className="flex justify-between">
+            <p className="font-bold text-lg">{item?.name}</p>
+            <div className="cursor-pointer">
+              <HeartIcon fill="#ED3F3F" />
+            </div>
           </div>
+          <p className="font-bold text-sm text-[#90A3BF]">{item?.type}</p>
         </div>
-        <p className="font-bold text-sm text-[#90A3BF]">{item?.type}</p>
-        <div
-          className="mobile:flex mobile:flex-row tablet:flex tablet:flex-col mt-[50px] cursor-pointer"
-          onClick={() => router.push("/car-detail")}
-        >
-          <Image
-            src={item.imageUrl}
-            alt={item.name}
-            width={breakpoint === "mobile" ? 160 : 248}
-            height={breakpoint === "mobile" ? 64 : 100}
-            priority
-          />
-          <div className="mobile:flex mobile:flex-col tablet:flex tablet:flex-row justify-between tablet:mt-9 mobile:ml-[50px] tablet:ml-0 text-[#90A3BF] text-sm">
-            <div className="flex items-center gap-[6px]">
-              <GasStationIcon />
-              <p className="mobile:text-xs tablet:text-md">{item.gas}</p>
-            </div>
-            <div className="flex items-center gap-[6px]">
-              <ManualCarIcon />
-              <p className="mobile:text-xs tablet:text-md">{item.gearType}</p>
-            </div>
-            <div className="flex items-center gap-[6px]">
-              <TwoUserIcon />
-              <p className="mobile:text-xs tablet:text-md">
-                {item.capacity} People
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="flex justify-between items-center mt-6">
-          <div className="flex items-center">
-            <p className="text-md font-bold">{item.price} /</p>
-            <p className="text-sm text-[#90A3BF]">day</p>
-          </div>
-          <div>
-            <Button
-              text="Rent Now"
-              variant="medium"
-              type="primary"
-              onClick={() => {}}
+        <div>
+          <div
+            className="mobile:flex mobile:flex-row tablet:flex tablet:flex-col mt-[50px] cursor-pointer"
+            onClick={() => router.push("/car-detail")}
+          >
+            <Image
+              src={item.imageUrl}
+              alt={item.name}
+              width={breakpoint === "mobile" ? 160 : 248}
+              height={breakpoint === "mobile" ? 64 : 100}
+              priority
             />
+            <div className="mobile:flex mobile:flex-col tablet:flex tablet:flex-row justify-between tablet:mt-9 mobile:ml-[50px] tablet:ml-0 text-[#90A3BF] text-sm">
+              <div className="flex items-center gap-[6px]">
+                <GasStationIcon />
+                <p className="mobile:text-xs tablet:text-md">{item.gas}</p>
+              </div>
+              <div className="flex items-center gap-[6px]">
+                <ManualCarIcon />
+                <p className="mobile:text-xs tablet:text-md">{item.gearType}</p>
+              </div>
+              <div className="flex items-center gap-[6px]">
+                <TwoUserIcon />
+                <p className="mobile:text-xs tablet:text-md">
+                  {item.capacity} People
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-between items-center mt-6">
+            <div className="flex items-center">
+              <p className="text-md font-bold">{item.price} /</p>
+              <p className="text-sm text-[#90A3BF]">day</p>
+            </div>
+            <div>
+              <Button
+                text="Rent Now"
+                variant="medium"
+                type="primary"
+                onClick={() => {}}
+              />
+            </div>
           </div>
         </div>
       </div>
